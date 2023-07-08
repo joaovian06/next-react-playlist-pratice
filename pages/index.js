f55cbe3a862a955774df825de074a92041ecabdc
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-08 03:17:56 -0300

    update home

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
-import { useState } from "react";
-
 function Home() {
   return <Counter />;
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  const addCount = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <div>{count}</div>
-      <button onClick={addCount}>Add</button>
-    </div>
-  );
 }
 
 export default Home;
