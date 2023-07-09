26a28d19462d5d05731c190c088804d0f3e87dac
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-09 16:28:56 -0300

    add api secret keys

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+  const apiSecret = process.env.API_SECRET;
   const dynamicDate = new Date();
+
+  const subscribersResponse = await fetch(`api-endpoint-with=${apiSecret}`);
+  const subscribersResponseJson = await subscribersResponse.json();
+  const subscribers = subscribersResponseJson;
 
   response.json({
     date: dynamicDate.toUTCString(),
+    subscribers: subscribers,
   });
 }
 
