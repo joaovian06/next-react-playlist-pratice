5677925d7688c089293cefd31fa68dfc50c12bd8
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-09 14:51:28 -0300

    adding pages about and link component from next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link>
+        <a href="sobre">Acessar página Sobre</a>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link>
+        <a href="/">Acessar página Home</a>
+      </Link>
+    </div>
+  );
+}
+
+export default About;
