2fe3b5131cceaebc35593d07d4fc3f37ef6ec8aa
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-09 16:36:59 -0300

    fix: remove test features for api endpoint

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,16 +1,16 @@
 async function tempo(request, response) {
-  const apiSecret = process.env.API_SECRET;
+  //   const apiSecret = process.env.API_SECRET;
   const dynamicDate = new Date();
 
-  const subscribersResponse = await fetch(`api-endpoint-with=${apiSecret}`);
-  const subscribersResponseJson = await subscribersResponse.json();
-  const subscribers = subscribersResponseJson;
+  //   const subscribersResponse = await fetch(`api-endpoint-with=${apiSecret}`);
+  //   const subscribersResponseJson = await subscribersResponse.json();
+  //   const subscribers = subscribersResponseJson;
 
   response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
 
   response.json({
     date: dynamicDate.toUTCString(),
-    subscribers: subscribers,
+    // subscribers: subscribers,
   });
 }
 
