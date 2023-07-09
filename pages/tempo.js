c0380426f09a65895e6a0f0c413a7787d3911e14
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-09 15:22:37 -0300

    testing static and dynamic rendering

---
+++ pages/tempo.js
@@ -0,0 +1,24 @@
+function Tempo(props) {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toUTCString();
+
+  return (
+    <div>
+      <div>{dynamicDateString} (dinâmico)</div>
+      <div>{props.staticDateString} (estático)</div>
+    </div>
+  );
+}
+
+export function getStaticProps() {
+  const staticDate = new Date();
+  const staticDateString = staticDate.toUTCString();
+
+  return {
+    props: {
+      staticDateString,
+    },
+  };
+}
+
+export default Tempo;
